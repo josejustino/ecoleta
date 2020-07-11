63b9a9fb1fb01d7e34519bdf08667d3445fc9a40
José <josejustinno@gmail.com>
2020-07-11 11:46:11 -0300

    add: alter url base uploads

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `exp://md-cua.anonymous.mobile.exp.direct:80/uploads/${item.image}`,
+                image_url: `http://localhost:3333/uploads/${item.image}`,
             };
         });
         return response.json(serializedItems);
